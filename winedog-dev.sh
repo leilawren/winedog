dab05abe2e24fe9058c5cf51f240d2a47483547e
leilawren <153777849+leilawren@users.noreply.github.com>
2025-07-12 17:49:55 -0700

    Update winedog-dev.sh

--- winedog-dev.sh
+++ winedog-dev.sh
@@ -150,7 +150,7 @@ EOF
 chmod +x runner.sh
 chmod +x winetricks.sh
 
-echo -e "${GREEN}Okay! Setup complete - Try launching your game from runner.sh${RESET}"
+echo -e "${GREEN}Setup complete - Try launching your game from runner.sh${RESET}"
 echo -e "${PINK}Winedog loves you${RESET}"
 
 exit
